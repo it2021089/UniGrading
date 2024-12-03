2985194db9804e4affc3def84389b3bfb16c87c4
Tzortz Roufail <it2021089@hua.gr>
2024-12-03 02:19:23 +0200

    few styling changes

--- UniGrading/subjects/views.py
+++ UniGrading/subjects/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Subject, Category
-from .forms import SubjectForm, CategoryForm
+from django.core.paginator import Paginator
+from .models import Subject
+from .forms import SubjectForm
 
 @login_required
 def my_subjects(request):
-    subjects = Subject.objects.all()
+    subjects_list = Subject.objects.all()
+    paginator = Paginator(subjects_list, 6)  # Show 6 subjects per page
+
+    page_number = request.GET.get('page')
+    subjects = paginator.get_page(page_number)
+
     return render(request, 'my_subjects.html', {'subjects': subjects})
 
 @login_required
@@ -16,10 +22,6 @@ def create_subject(request):
             subject = form.save(commit=False)
             subject.professor = request.user
             subject.save()
-            # Create default categories
-            Category.objects.create(subject=subject, name='Lectures')
-            Category.objects.create(subject=subject, name='Assignments')
-            Category.objects.create(subject=subject, name='Tests')
             return redirect('my_subjects')
     else:
         form = SubjectForm()
@@ -28,14 +30,4 @@ def create_subject(request):
 @login_required
 def subject_detail(request, pk):
     subject = get_object_or_404(Subject, pk=pk)
-    categories = subject.categories.all()
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            category = form.save(commit=False)
-            category.subject = subject
-            category.save()
-            return redirect('subject_detail', pk=subject.pk)
-    else:
-        form = CategoryForm()
-    return render(request, 'subject_detail.html', {'subject': subject, 'categories': categories, 'form': form})
+    return render(request, 'subject_detail.html', {'subject': subject})
