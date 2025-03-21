4333f888c718f9fd099bb64b1b7e78e4005adaeb
George Roufaeil <it2021089@hua.gr>
2025-03-21 19:04:27 +0200

    made changes to assignments

--- UniGrading/assignments/urls.py
+++ UniGrading/assignments/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import AssignmentCreateView, AssignmentListView
+from .views import AssignmentDetailView
+from .views import AssignmentUpdateView
+from . import views
 
 app_name = "assignments"
 
 urlpatterns = [
     path('<int:subject_id>/', AssignmentListView.as_view(), name='assignment_list'),
     path('<int:subject_id>/create/', AssignmentCreateView.as_view(), name='create'),  
+    path('assignment/<int:pk>/', AssignmentDetailView.as_view(), name='assignment_detail'),
+    path('assignments/<int:assignment_id>/delete/', views.delete_assignment, name='delete_assignment'),
+    path('assignments/<int:assignment_id>/edit/', AssignmentUpdateView.as_view(), name='assignment_edit'),
 
 ]
--- UniGrading/assignments/views.py
+++ UniGrading/assignments/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView
 from subjects.models import Subject
 from .models import Assignment
 from .forms import AssignmentForm
-from UniGrading.mixin import BreadcrumbMixin  # Import BreadcrumbMixin
+from django.views.generic import DetailView
+from UniGrading.mixin import BreadcrumbMixin  
+from django.views.generic import UpdateView
+
 
 class AssignmentListView(LoginRequiredMixin, BreadcrumbMixin, ListView):
     model = Assignment
@@ -38,14 +42,83 @@ class AssignmentCreateView(LoginRequiredMixin, BreadcrumbMixin, CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         subject = get_object_or_404(Subject, pk=self.kwargs["subject_id"])
-        context["subject"] = subject  # Ensure subject is passed to the template
+        context["subject"] = subject
         return context
+
+    def get_breadcrumbs(self):
+        subject = get_object_or_404(Subject, pk=self.kwargs["subject_id"])
+        return [
+            ("Dashboard", reverse_lazy("users:professor_dashboard") if self.request.user.role == "professor" else reverse_lazy("users:student_dashboard")),
+            ("My Subjects", reverse_lazy("subjects:my_subjects")),
+            (subject.name, reverse_lazy("subjects:subject_detail", kwargs={"pk": subject.pk})),
+            ("Assignments", reverse_lazy("assignments:assignment_list", kwargs={"subject_id": subject.pk})),
+            ("Create Assignment", self.request.path),
+        ]
 
     def form_valid(self, form):
         subject = get_object_or_404(Subject, pk=self.kwargs["subject_id"])
         form.instance.professor = self.request.user
-        form.instance.subject = subject  # Ensure the assignment is linked to the correct subject
+        form.instance.subject = subject
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy("assignments:assignment_list", kwargs={"subject_id": self.kwargs["subject_id"]})
+
+
+class AssignmentDetailView(LoginRequiredMixin, BreadcrumbMixin, DetailView):
+    model = Assignment
+    template_name = "assignment_detail.html"
+    context_object_name = "assignment"
+
+    def get_breadcrumbs(self):
+        assignment = self.get_object()
+        return [
+            ("Dashboard", reverse_lazy("users:professor_dashboard") if self.request.user.role == "professor" else reverse_lazy("users:student_dashboard")),
+            ("My Subjects", reverse_lazy("subjects:my_subjects")),
+            (assignment.subject.name, reverse_lazy("subjects:subject_detail", kwargs={"pk": assignment.subject.pk})),
+            ("Assignments", reverse_lazy("assignments:assignment_list", kwargs={"subject_id": assignment.subject.pk})),
+            (assignment.title, self.request.path),
+        ]
+    
+
+@login_required
+def delete_assignment(request, assignment_id):
+    assignment = get_object_or_404(Assignment, pk=assignment_id)
+
+    if request.user != assignment.professor:
+        return redirect("assignments:assignment_list", subject_id=assignment.subject.pk)
+
+    if request.method == "POST":
+        subject_id = assignment.subject.pk
+        assignment.delete()
+        return redirect("assignments:assignment_list", subject_id=subject_id)   
+class AssignmentUpdateView(LoginRequiredMixin, BreadcrumbMixin, UpdateView):
+    model = Assignment
+    form_class = AssignmentForm
+    template_name = "assignment_form.html"
+
+    def get_object(self):
+        return get_object_or_404(Assignment, pk=self.kwargs['assignment_id'])
+
+    def get_success_url(self):
+        return reverse_lazy('assignments:assignment_detail', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["subject"] = self.object.subject
+        return context
+
+    def get_breadcrumbs(self):
+        assignment = self.get_object()
+        subject = assignment.subject
+
+        dashboard_url = reverse_lazy("users:professor_dashboard") if self.request.user.role == "professor" else reverse_lazy("users:student_dashboard")
+
+        return [
+            ("Dashboard", dashboard_url),
+            ("My Subjects", reverse_lazy("subjects:my_subjects")),
+            (subject.name, reverse_lazy("subjects:subject_detail", kwargs={"pk": subject.pk})),
+            ("Assignments", reverse_lazy("assignments:assignment_list", kwargs={"subject_id": subject.pk})),
+            (assignment.title, reverse_lazy("assignments:assignment_detail", kwargs={"pk": assignment.pk})),
+            ("Edit", self.request.path),
+        ]
